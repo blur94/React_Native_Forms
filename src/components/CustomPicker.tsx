e5f21e207e3240014cdaf3e8169ed6d0d955a1dc
Gilead Odo <odogilead@gmail.com>
2025-01-13 01:59:04 +0100

    Refactor CustomPicker component for improved styling and iOS compatibility

--- src/components/CustomPicker.tsx
+++ src/components/CustomPicker.tsx
@@ -1,11 +1,11 @@
-import { ComponentProps } from 'react';
-import { useController } from 'react-hook-form';
-import { View, Text } from 'react-native';
-import RNPickerSelect from 'react-native-picker-select';
+import { ComponentProps } from "react";
+import { useController } from "react-hook-form";
+import { View, Text } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
 
 type CustomPicker = {
   name: string;
-} & Omit<ComponentProps<typeof RNPickerSelect>, 'onValueChange'>;
+} & Omit<ComponentProps<typeof RNPickerSelect>, "onValueChange">;
 
 export default function CustomPicker({ name, ...pickerProps }: CustomPicker) {
   const {
@@ -20,21 +20,22 @@ export default function CustomPicker({ name, ...pickerProps }: CustomPicker) {
         value={value}
         onValueChange={onChange}
         onClose={onBlur}
+        textInputProps={{ pointerEvents: "none" }} //This fixes the issue where picker won't open on iOs
         style={{
           viewContainer: {
             marginTop: 4,
             marginBottom: 4,
           },
           inputIOS: {
-            borderColor: error ? 'crimson' : 'gainsboro',
+            borderColor: error ? "crimson" : "gainsboro",
             borderWidth: 1,
-            width: '100%',
+            width: "100%",
             padding: 10,
             borderRadius: 5,
           },
         }}
       />
-      <Text style={{ color: 'crimson', height: 17 }} numberOfLines={1}>
+      <Text style={{ color: "crimson", height: 17 }} numberOfLines={1}>
         {error?.message}
       </Text>
     </View>
